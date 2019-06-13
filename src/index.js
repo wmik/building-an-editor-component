70e5baf9110d16618ce63deb94b9dc36bfac8519
wmik <williemik.wmik@gmail.com>
2019-06-13 08:03:12 +0300

    feat: post component
    - UI setup
    - Add state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  Button,
+  Divider,
+  Form,
+  Grid,
+  Header,
+  Segment
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
-  return <div>application</div>;
+  const [hasText, setHasText] = React.useState(false);
+  const handleChange = React.useCallback(
+    e => {
+      if (e.currentTarget.textLength > 0 && !hasText) {
+        setHasText(true);
+      } else if (e.currentTarget.textLength === 0) {
+        setHasText(false);
+      }
+    },
+    [hasText, setHasText]
+  );
+  return (
+    <Grid stackable>
+      <Grid.Row centered>
+        <Grid.Column width={8}>
+          <Segment>
+            <Header content="New Post" />
+            <Divider />
+            <Form>
+              <Form.TextArea
+                placeholder="Type your post"
+                onChange={handleChange}
+              />
+              <Button basic circular icon="image outline" />
+              <Button basic circular icon="attach" />
+              <Button basic circular icon="smile" />
+              <Button
+                type="submit"
+                floated="right"
+                disabled={!hasText}
+                color={hasText ? "green" : ""}
+              >
+                Post
+              </Button>
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
 
 const rootElement = document.getElementById("root");
